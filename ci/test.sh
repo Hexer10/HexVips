37b8b19eec16696ba6aebe4189923be48bf18511
Hexah <hexer504@gmail.com>
2017-09-25 14:43:53 +0200

    Travis

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,7 @@
 set -ev
 
 echo "Download und extract sourcemod"
-wget -q "http://www.sourcemod.net/latest.php?version=$1&os=linux" -O sourcemod.tar.gz
+wget -q "http://www.sourcemod.net/latest.php?version=1.8&os=linux" -O sourcemod.tar.gz
 tar -xzf sourcemod.tar.gz
 
 echo "Give compiler rights for compile"

8183d4a4a2459c2479255c251965354c0f324d0e
Hexah <hexer504@gmail.com>
2017-09-25 14:37:15 +0200

    Travis again

---
+++ ci/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+
+echo "Download und extract sourcemod"
+wget -q "http://www.sourcemod.net/latest.php?version=$1&os=linux" -O sourcemod.tar.gz
+tar -xzf sourcemod.tar.gz
+
+echo "Give compiler rights for compile"
+chmod +x addons/sourcemod/scripting/spcomp
+
+echo "Compile VipBonus plugin"
+addons/sourcemod/scripting/spcomp -E -v0 addons/sourcemod/scripting/VipBonus.sp

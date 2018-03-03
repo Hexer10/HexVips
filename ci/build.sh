5e8dd1f6575630a68049fb90e63538577c86f48c
Hexah <hexer504@gmail.com>
2018-03-03 10:43:46 +0100

    Add Phrases to Travis build

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,14 @@ set -ev
 
 TAG=$1
 
+
+echo "Create clean plugins folder"
+mkdir -p build/addons/sourcemod/scripting/include
+mkdir build/addons/sourcemod/plugins
+
+echo "Move Phrases"
+mv addons/sourcemod/translations
+
 echo "Download und extract sourcemod"
 wget "http://www.sourcemod.net/latest.php?version=1.8&os=linux" -O sourcemod.tar.gz
 tar -xzf sourcemod.tar.gz
@@ -19,21 +27,11 @@ done
 
 addons/sourcemod/scripting/compile.sh VipBonus.sp
 
-echo "Remove plugins folder if exists"
-if [ -d "addons/sourcemod/plugins" ]; then
-  rm -r addons/sourcemod/plugins
-fi
-
-echo "Create clean plugins folder"
-mkdir -p build/addons/sourcemod/scripting/include
-mkdir build/addons/sourcemod/plugins
-
 echo "Move plugins files to their folder"
 mv addons/sourcemod/scripting/include/vipbonus.inc build/addons/sourcemod/scripting/include
 mv addons/sourcemod/scripting/VipBonus.sp build/addons/sourcemod/scripting
 mv addons/sourcemod/scripting/VipMenu.sp build/addons/sourcemod/scripting
 mv addons/sourcemod/scripting/compiled/VipBonus.smx build/addons/sourcemod/plugins
-
 
 echo "Compress the plugin"
 mv LICENSE build/
